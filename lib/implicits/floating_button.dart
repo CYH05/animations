54b9478d5388cfe4f3ba5e3d60597091aaf6fb34
Claudio <claudiohanada@hotmail.com>
2022-02-07 20:06:07 -0300

    Removing unecessary var (scales) to use alignment enums, and turn the scaffold body to only one widget.

--- lib/implicits/floating_button.dart
+++ lib/implicits/floating_button.dart
@@ -8,10 +8,7 @@ class ImplicitFloatingButton extends StatefulWidget {
 }
 
 class _ImplicitFloatingButtonState extends State<ImplicitFloatingButton> {
-  bool _withAnimatedContainers = true;
   bool _expanded = false;
-  double _scaleX = 1.0;
-  double _scaleY = 1.0;
   double _width = 40.0;
   double _height = 40.0;
   double _rounded = 100.00;
@@ -23,63 +20,35 @@ class _ImplicitFloatingButtonState extends State<ImplicitFloatingButton> {
         backgroundColor: Colors.blue,
         title: const Text("Desafio do botão flutuante"),
       ),
-      body: _withAnimatedContainers
-          ? GestureDetector(
-              onTap: () {
-                setState(() {
-                  if (_expanded) {
-                    _scaleX++;
-                    _scaleY += 2;
-                    _width -= 50;
-                    _rounded = 100;
-                  } else {
-                    _scaleX--;
-                    _scaleY -= 2;
-                    _width += 50;
-                    _rounded = 0;
-                  }
-                  _expanded = !_expanded;
-                });
-              },
-              child: AnimatedContainer(
-                duration: Duration(seconds: 1),
-                alignment: Alignment(_scaleX, _scaleY),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1),
-                    width: _width,
-                    height: _height,
-                    decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(_rounded)),
-                  ),
-                ),
-              ),
-            )
-          : Stack(
-              children: [
-                AnimatedPositioned(
-                  width: _expanded ? 40 : 90,
-                  height: _height,
-                  right: _expanded ? 15 : 150,
-                  bottom: _expanded ? 15 : 500,
-                  child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _expanded = !_expanded;
-                        });
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius:
-                                BorderRadius.circular(_expanded ? 100 : 0)),
-                      )),
-                  duration: Duration(seconds: 1),
-                ),
-              ],
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_expanded) {
+              _width = _width - 50;
+              _rounded = 100;
+            } else {
+              _width = _width + 50;
+              _rounded = 0;
+            }
+            _expanded = !_expanded;
+          });
+        },
+        child: AnimatedContainer(
+          duration: Duration(seconds: 1),
+          alignment: _expanded ? Alignment.topCenter : Alignment.bottomRight,
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              width: _width,
+              height: _height,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(_rounded)),
             ),
+          ),
+        ),
+      ),
     );
   }
 }
