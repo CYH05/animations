849bcfd52c17d8d271babd85093461ea33ea812b
Claudio <claudiohanada@hotmail.com>
2022-02-07 18:03:37 -0300

    TileList with implicit animination

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,144 +20,137 @@ class _HomePageState extends State<HomePage> {
     return Container(
       color: Colors.white,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            child: _implicitMenuChecked
-                ? Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            _implicitMenuChecked = false;
-                          });
-                        },
-                        child: const Text("Animações implícitas"),
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.grey,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: RaisedButton(
-                          color: Colors.blue,
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    const ImplicitFloatingButton(),
-                              ),
-                            );
-                          },
-                          child: const Text(
-                            "Floating button",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: RaisedButton(
-                          color: Colors.blue,
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const ImplicitExpansionTile()),
-                            );
-                          },
-                          child: const Text(
-                            "Scroll view",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                : Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            _implicitMenuChecked = true;
-                            if (_controlledMenuChecked)
-                              _controlledMenuChecked = false;
-                          });
-                        },
-                        child: const Text("Animações implícitas"),
-                      ),
-                    ],
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: SizedBox(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _implicitMenuChecked = !_implicitMenuChecked;
+                        if (_controlledMenuChecked)
+                          _controlledMenuChecked = false;
+                      });
+                    },
+                    child: const Text("Animações implícitas"),
                   ),
+                  ClipRect(
+                    child: AnimatedAlign(
+                      alignment: _implicitMenuChecked
+                          ? Alignment.topCenter
+                          : Alignment.bottomCenter,
+                      duration: Duration(milliseconds: 500),
+                      heightFactor: _implicitMenuChecked ? 1 : 0,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: RaisedButton(
+                              color: Colors.blue,
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ImplicitFloatingButton()));
+                              },
+                              child: const Text(
+                                "Floating button",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: RaisedButton(
+                              color: Colors.blue,
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ImplicitExpansionTile()));
+                              },
+                              child: const Text(
+                                "Scroll view",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           SizedBox(
-            child: _controlledMenuChecked
-                ? Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            _controlledMenuChecked = false;
-                          });
-                        },
-                        child: const Text("Animações controladas"),
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.grey,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: RaisedButton(
-                          color: Colors.blue,
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const ControlledFloatingButton()));
-                          },
-                          child: const Text(
-                            "Flaoting button",
-                            style: TextStyle(
-                              color: Colors.white,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _controlledMenuChecked = !_controlledMenuChecked;
+                      if (_implicitMenuChecked) _implicitMenuChecked = false;
+                    });
+                  },
+                  child: const Text("Animações controladas"),
+                ),
+                ClipRect(
+                  child: AnimatedAlign(
+                    alignment: _controlledMenuChecked
+                        ? Alignment.topCenter
+                        : Alignment.bottomCenter,
+                    duration: Duration(milliseconds: 500),
+                    heightFactor: _controlledMenuChecked ? 1 : 0,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: RaisedButton(
+                            color: Colors.blue,
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ControlledFloatingButton()));
+                            },
+                            child: const Text(
+                              "Floating button",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: RaisedButton(
-                          color: Colors.blue,
-                          onPressed: () {},
-                          child: const Text(
-                            "Scroll view",
-                            style: TextStyle(
-                              color: Colors.white,
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: RaisedButton(
+                            color: Colors.blue,
+                            onPressed: () {},
+                            child: const Text(
+                              "Scroll view",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  )
-                : Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            _controlledMenuChecked = true;
-                            if (_implicitMenuChecked)
-                              _implicitMenuChecked = false;
-                          });
-                        },
-                        child: const Text("Animações controladas"),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/implicits/expansion_tile.dart
+++ lib/implicits/expansion_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 class ImplicitExpansionTile extends StatefulWidget {
   const ImplicitExpansionTile({Key? key}) : super(key: key);
@@ -9,13 +8,22 @@ class ImplicitExpansionTile extends StatefulWidget {
 }
 
 class _ImplicitExpansionTileState extends State<ImplicitExpansionTile> {
-  bool _expanded = false;
-  double _rotation = .0;
+  bool _expanded1 = false;
+  bool _expanded2 = false;
+  double _rotation1 = .0;
+  double _rotation2 = .0;
 
-  void _rotate() {
+  void _rotateFirst() {
     setState(() {
-      _rotation += 1.0 / 2.0;
-      _expanded = !_expanded;
+      _rotation1 += 1.0 / 2.0;
+      _expanded1 = !_expanded1;
+    });
+  }
+
+  void _rotateSecond() {
+    setState(() {
+      _rotation2 += 1.0 / 2.0;
+      _expanded2 = !_expanded2;
     });
   }
 
@@ -27,7 +35,7 @@ class _ImplicitExpansionTileState extends State<ImplicitExpansionTile> {
       ),
       body: Column(children: [
         GestureDetector(
-          onTap: () => _rotate(),
+          onTap: () => _rotateFirst(),
           child: Column(
             children: [
               Row(
@@ -43,23 +51,66 @@ class _ImplicitExpansionTileState extends State<ImplicitExpansionTile> {
                   Padding(
                     padding: const EdgeInsets.all(10),
                     child: AnimatedRotation(
-                      turns: _rotation,
+                      turns: _rotation1,
                       duration: const Duration(milliseconds: 100),
                       child: const Icon(Icons.arrow_drop_down),
                     ),
                   ),
                 ],
               ),
-              AnimatedAlign(
-                alignment:
-                    _expanded ? Alignment.topCenter : Alignment.bottomCenter,
-                duration: Duration(seconds: 1),
-                heightFactor: 0,
-                child: Column(
-                  children: [
-                    Icon(Icons.zoom_out_map_outlined),
-                    Text("Lorem ipsum"),
-                  ],
+              ClipRect(
+                child: AnimatedAlign(
+                  alignment:
+                      _expanded1 ? Alignment.topCenter : Alignment.bottomCenter,
+                  duration: Duration(milliseconds: 500),
+                  heightFactor: _expanded1 ? 1 : 0,
+                  child: Column(
+                    children: const [
+                      Icon(Icons.zoom_out_map_outlined),
+                      Text("Lorem ipsum"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () => _rotateSecond(),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "My expansion tile 1",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: AnimatedRotation(
+                      turns: _rotation2,
+                      duration: const Duration(milliseconds: 100),
+                      child: const Icon(Icons.arrow_drop_down),
+                    ),
+                  ),
+                ],
+              ),
+              ClipRect(
+                child: AnimatedAlign(
+                  alignment:
+                      _expanded2 ? Alignment.topCenter : Alignment.bottomCenter,
+                  duration: Duration(milliseconds: 500),
+                  heightFactor: _expanded2 ? 1 : 0,
+                  child: Column(
+                    children: const [
+                      Icon(Icons.zoom_out_map_outlined),
+                      Text("Lorem ipsum"),
+                    ],
+                  ),
                 ),
               ),
             ],
